524a8bbf930c076deefafbff75853e074062a45e
DmitriyCherkaska <151064884+DmitriyCherkaska@users.noreply.github.com>
2024-05-13 03:04:49 +0300

    asdasd

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,9 +1,9 @@
-import Profile from "./Profile/Profile";
-import FriendList from "./FriendList/FriendList";
-import TransactionHistory from "./TransactionHistory/TransactionHistory";
-import transactions from "../transactions.json";
-import userData from "../userData.json";
-import friends from "../friends.json";
+import Profile from "../Profile/Profile";
+import FriendList from "../FriendList/FriendList";
+import TransactionHistory from "../TransactionHistory/TransactionHistory";
+import transactions from "../../transactions.json";
+import userData from "../../userData.json";
+import friends from "../../friends.json";
 import "./App.css";
 
 const App = () => {
