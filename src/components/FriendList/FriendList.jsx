1e0e95ef3dbde52dfdb26388a29be892463863e7
DmitriyCherkaska <151064884+DmitriyCherkaska@users.noreply.github.com>
2024-05-14 17:33:01 +0300

    фівфів

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,18 +1,7 @@
 import some from "./FriendList.module.css";
+import FriendListItem from "../FriendListItem/FriendListItem";
 
 // console.log("some :>>", some);
-
-const FriendListItem = ({ avatar, name, isOnline }) => {
-  return (
-    <div>
-      <img src={avatar} alt="Avatar" width="90" />
-      <p className={some.name}>{name}</p>
-      <p className={some.status} style={{ color: isOnline ? "green" : "red" }}>
-        {isOnline ? "Online" : "Offline"}
-      </p>
-    </div>
-  );
-};
 
 const FriendList = ({ friends }) => {
   return (
